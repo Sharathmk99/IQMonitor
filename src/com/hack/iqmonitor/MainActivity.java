b03bd38b36c4cec86d0e1181ef212451b78139bf
Sharath M K <sharathmk99@gmail.com>
2015-01-24 15:55:37 +0530

    Second

--- src/com/hack/iqmonitor/MainActivity.java
+++ src/com/hack/iqmonitor/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 public class MainActivity extends ActionBarActivity {
 
 	Button button;
-
+	Button button2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
