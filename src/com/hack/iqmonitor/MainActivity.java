a798a696c327eb0e540caad1423ed20ff28ad748
Sharath M K <sharathmk99@gmail.com>
2015-01-24 17:04:35 +0530

    Update

--- src/com/hack/iqmonitor/MainActivity.java
+++ src/com/hack/iqmonitor/MainActivity.java
@@ -2,16 +2,9 @@ package com.hack.iqmonitor;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
-	Button button;
-	Button button2;
-	Button button3;
-	Button button4;
-	Button button5;
-	Button button6;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
