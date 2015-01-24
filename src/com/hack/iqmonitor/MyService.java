e5aba512d4af5a86e1c4f4602db6aacc66aac273
Sharath M K <sharathmk99@gmail.com>
2015-01-24 17:07:10 +0530

    Up

--- src/com/hack/iqmonitor/MyService.java
+++ src/com/hack/iqmonitor/MyService.java
@@ -3,9 +3,11 @@ package com.hack.iqmonitor;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Button;
 
 public class MyService extends Service {
 
+	Button button;
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
