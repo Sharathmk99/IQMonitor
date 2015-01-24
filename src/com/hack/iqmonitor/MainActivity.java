21b18f816a41f071101aa97267b5841e7b69031b
Sharath M K <sharathmk99@gmail.com>
2015-01-24 15:53:59 +0530

    Second

--- src/com/hack/iqmonitor/MainActivity.java
+++ src/com/hack/iqmonitor/MainActivity.java
@@ -1,11 +1,12 @@
 package com.hack.iqmonitor;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+
+	Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,22 +14,4 @@ public class MainActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
